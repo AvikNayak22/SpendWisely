bcc891aac2e6f755d432849689f170aade2ed04d
AvikNayak <nayakavik19@gmail.com>
2024-06-06 17:24:51 +0530

    minor changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), viteCompression()],
   server: {
     proxy: {
